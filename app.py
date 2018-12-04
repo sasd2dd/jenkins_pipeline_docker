33b0f720fe1a02bb1d9140002056158605264cea
TimG <timothy.gonda@gmail.com>
2018-12-04 13:40:23 -0500

    asdf

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_redis import FlaskRedis
 
 app = Flask(__name__)
-app.config['REDIS_URL'] = 'redis://redis:6380/0'
+app.config['REDIS_URL'] = 'redis://redis:6379/0'
 
 redis = FlaskRedis(app)
 
